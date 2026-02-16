d7050bc38488dc2951c9f3a7be28385eb8df6028
Sruthi Sami <sheisruthisami@gmail.com>
2026-02-16 22:50:24 +0530

    wip: ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import Header from "@/components/header";
+import Footer from "@/components/footer"
 import { Geist, Geist_Mono } from "next/font/google";
 import Providers from "@/components/providers";
 import "./globals.css";
@@ -32,6 +33,7 @@ export default function RootLayout({
         <Providers>
           <Header />
           {children}
+          <Footer/>
         </Providers>
       </body>
     </html>
