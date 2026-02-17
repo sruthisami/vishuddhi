39c3058fe4fddae22132514c81d8488500eb1d41
vkrafted <vaishnavikoppakula@gmail.com>
2026-02-17 14:07:52 +0530

    tsConfig

---
+++ backend/src/index.ts
@@ -0,0 +1,14 @@
+const express = require('express');
+import { Request, Response } from "express";
+const app = express();
+const PORT = 3001
+
+app.use(express.json());
+
+app.get("/",(req: Request, res: Response)=>{
+    res.send("Hello World")
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`)
+})
